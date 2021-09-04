fdd946a1b0958b66eef0ce3dcaf37175fbf834b5
sofien njeimi <7sofien@gmail.com>
2021-09-04 13:41:01 +0100

    initial commit

---
+++ src/chapter1/Principale.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class Principale {
+  
+	public static void main(String args[]) {
+		Etudiant e1 = new Etudiant();
+		Etudiant e2 = new Etudiant(22, "all");
+		
+		System.out.println();
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module oca_chapter1 {
+}
